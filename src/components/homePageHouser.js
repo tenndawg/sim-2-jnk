c37892c2b7154930692c2166777196f08d99972e
Kevin Patterson <Home@Kevins-MacBook.local>
2017-10-28 12:32:12 -0600

    Addin New Files
    :q
    cd
    cd ..
    
    :wq

---
+++ src/components/homePageHouser.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import '../css/homePageHouser.css';
+
+class SHELFIE_HOME extends Component {
+    render() {
+        return(
+            <div className='shelfie_Body' >
+                
+                <div className='top_bar'>
+                    <img src={SHELFIE_ICON} alt='Shelfie Icon' />
+                    SHELFIE
+                </div>
+
+                <div className='buttonBox'>
+                    <Link to="/shelf_A" >
+                        <button className='buttonBoxB'>Shelf A</button>
+                    </Link>
+                    <Link to="/shelf_B" >
+                        <button className='buttonBoxB'>Shelf B</button>
+                    </Link>
+                    <Link to="/shelf_C" >
+                        <button className='buttonBoxB'>Shelf C</button>
+                    </Link>
+                    <Link to="/shelf_D" >
+                        <button className='buttonBoxB'>Shelf D</button>
+                    </Link>
+                </div>
+                    
+            </div>
+        );
+    }
+}
+
+export default SHELFIE_HOME;
