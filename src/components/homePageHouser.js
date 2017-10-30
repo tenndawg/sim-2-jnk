981cd51bef72a7f89c2fc803801f19cd3a097e35
Kevin Patterson <Home@Kevins-MacBook.local>
2017-10-29 21:04:56 -0600

    This is the home page complete and ready for middle and backend code structure

--- src/components/homePageHouser.js
+++ src/components/homePageHouser.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
+import HOUSE_ICON from '../images/houseICON.png';
+
 
 import '../css/homePageHouser.css';
 
 class SHELFIE_HOME extends Component {
     render() {
         return(
-            <div className='shelfie_Body' >
-                
-                <div className='top_bar'>
-                    <img src={SHELFIE_ICON} alt='Shelfie Icon' />
-                    SHELFIE
+            <div className='login_Body' >
+
+                <div className='login_Body_Image'>
+                    <img src={ HOUSE_ICON } alt='Houser Logo Icon' />
                 </div>
 
-                <div className='buttonBox'>
-                    <Link to="/shelf_A" >
-                        <button className='buttonBoxB'>Shelf A</button>
-                    </Link>
-                    <Link to="/shelf_B" >
-                        <button className='buttonBoxB'>Shelf B</button>
-                    </Link>
-                    <Link to="/shelf_C" >
-                        <button className='buttonBoxB'>Shelf C</button>
-                    </Link>
-                    <Link to="/shelf_D" >
-                        <button className='buttonBoxB'>Shelf D</button>
-                    </Link>
+                <div className='login_ICON_Text'>
+                    Houser
+                </div>
+
+                <div className='login_Form'>
+                    &nbsp; Username:
+                    <br/>
+                    <input className='userButtonBox'
+                        type='text'
+                        /* Spaces '&nbsp;&nbsp;' added for better readability
+                        of text in the input space */
+                        value='&nbsp;&nbsp; Clint Eastwood'
+                    /> 
+                    <br/><br/>
+                    &nbsp; Password:
+                    <br/>
+                    <input className='passButtonBox'
+                        type='text'
+                        /* Spaces '&nbsp;&nbsp;' added for better readability
+                        of text in the input space */
+                        value='&nbsp;&nbsp; &bull;&bull;&bull;&bull;&bull;&bull;'
+                    /> 
+                </div>
+                <div className='logRegButtons' >
+                    <button
+                        className='userButton'
+                        onClick=''
+                        >
+                        Login
+                    </button>
+                    <button
+                        className='passButton'
+                        onClick=''
+                        >
+                        Register
+                    </button>
                 </div>
                     
             </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import HomePageHouser from './components/homePageHouser';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+<HomePageHouser />,
+document.getElementById('root'));
