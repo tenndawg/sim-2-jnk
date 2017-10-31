d2ea81a7c4b492632f57a3ecbd91ddefb39e0de0
Nickolas Tenney <ntenney@spillman.com>
2017-10-31 08:33:15 -0600

    changed file names

---
+++ src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import '../css/homePageHouser.css';
+
+class HouserHome extends Component {
+    render() {
+        return(    
+            <div>
+            <div className='shelfie_Body' >
+                <div className='top_bar'>
+                </div>
+            </div>
+            <div className='buttonBox'>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HouserHome;
