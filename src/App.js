92b7c0e3f7607d8cd513ed902adf5f8c0a453e54
Nickolas Tenney <ntenney@spillman.com>
2017-11-01 13:51:13 -0600

    Got routing working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Dashboard from './components/Dashboard';
+import Main from './components/Main';
+import homePageHouser from './components/homePageHouser';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard/>
+        <homePageHouser />
+        <Main />
+        <Dashboard />
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,22 @@
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={homePageHouser}/>
-      <Route path='/dashboard' component={Dashboard}/>
-    </Switch>
-  </main>
-)
+import homePageHouser from './homePageHouser';
+import Dashboard from './Dashboard';
+
+class Main extends Component {
+    
+    
+    render() {
+        return(
+        <main>
+            <Switch>
+            <Route exact path='/' component={homePageHouser}/>
+            <Route path='/dashboard' component={Dashboard}/>
+            </Switch>
+        </main>
+        );
+    }
+}
+
+export default Main;
