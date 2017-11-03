be2d2cbf82f7a9cec3d445cccd9e3bca327a8e2c
tenndawg <nickolas.tenney@gmail.com>
2017-11-02 20:43:53 -0600

    lots of changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Dashboard from './components/Dashboard';
 import homePageHouser from './components/homePageHouser';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import './app.css';
+import './index.css';
 
 class App extends Component {
   render() {
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
-
+import navicon from '../images/navicon.png';
 import '../css/dashboard.css';
+import '../index.css';
 
 class Dashboard extends Component {
     render() {
         return(    
-            <div className='body'>
-                <div className='top_bar'></div>
+            <div className="Dashboard__container">
+                <div className='top_bar'>
+                    <div className='Header__child'>
+                        <img src={ navicon } />
+                        <span className='Header__left_span open-sans-bold'>Houser</span>
+                    </div>
+                </div>
                 <div className='buttonBox'>
                     <div className='divBox'>
                         <p className='colorBox'><b>Add New Property</b></p>
